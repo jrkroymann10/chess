5e026090be01a82f3e8122bdbbcde42250a7e1d5
Joseph Kroymann <jrkroymann10@gmail.com>
2020-08-03 23:31:17 -0500

    updated intro text

--- lib/text_module.rb
+++ lib/text_module.rb
@@ -3,7 +3,7 @@ module GameText
     adheres to the common rules of chess (a quick google search will tell everything
     you need to know). to move a piece, simply type in the location of the piece,
     followed by a colon, and the location you would like to move the piece to.
-    for example, if you wanted to move a rook from b1 to a3, one would input b1:a3
+    for example, if you wanted to move a knight from b1 to a3, one would input b1:a3
     to execute the move. additionally, if one wants to view all of the possible moves
     a piece could make, simply input the location of the piece, and all of the possible
     moves will be highlighted. if one would like the game to be saved, simply input
